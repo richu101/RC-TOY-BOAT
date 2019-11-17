cba876d077391d173475bb2b6769abd8ccce20d3
richu101 <45835951+richu101@users.noreply.github.com>
2019-11-17 10:54:43 +0530

    left and right motation is also estabilished

--- maping_the_pwm/forward_and_backward/forward_and_backward.ino
+++ maping_the_pwm/forward_and_backward/forward_and_backward.ino
@@ -1,5 +1,5 @@
-const int ch1 = 9;//RX chanal 1
-const int ch2 = 10;//RX chanal 2
+const int ch1 = 10;//RX chanal 1
+const int ch2 = 9;//RX chanal 2
 const int ch3 = 11;//RX chanal 3
 const int motlf = 5;//If this pin is high then the left motor will rotate clockwise
 const int motlb = 6;//Left motor anti clockwise
@@ -23,58 +23,52 @@ Serial.begin(9600);
 void loop() {
 // put your main code here, to run repeatedly:
 ch1pwm = pulseIn(ch1,HIGH);
-ch3pwm = pulseIn(ch1,HIGH);
-ch2pwm = pulseIn(ch1,HIGH);
+ch3pwm = pulseIn(ch3,HIGH);
+ch2pwm = pulseIn(ch2,HIGH);
 //The signal from the tx is between 994 & 1981 so we map the sigmal in between 0 & 254
 mapch1pwm = map(ch1pwm,994,1981,0,254);
 mapch2pwm = map(ch2pwm,994,1981,0,254);
 mapch3pwm = map(ch3pwm,994,1981,0,254);
 //Serial.println(mapch1pwm);
 analogWrite(ch3,mapch3pwm);
-if(mapch2pwm<120||mapch2pwm>140)
+if(mapch1pwm<110||mapch1pwm>150)//if the value of chanal is between 120 and 140 then move forward 
 {
-if(mapch2pwm<125)
+if(mapch1pwm<120) 
 {
-switchchar="b";
-Serial.println("b");
-Serial.println(mapch2pwm);
+Serial.println(mapch1pwm);
 backward();
 }
-else if(mapch2pwm>130)
+else  if(mapch1pwm>140)
 {
-  switchchar="f";
-  Serial.println("f");
-   Serial.println(mapch2pwm);
+  Serial.println(mapch1pwm);
    forward();
+} 
+
+}
+else if(mapch2pwm>150||mapch2pwm<110)
+{
+if(mapch2pwm>140)
+{
+   Serial.println(mapch2pwm);
+   left();
+}
+else if(mapch2pwm<120)
+{
+Serial.println(mapch2pwm);
+Serial.println(mapch2pwm);
+right();
 }
 }
 else 
 {
   stope();
   Serial.println("sfb");
-}
-/*if(mapch1pwm<=130)
-{
-  switchchar="l";
-  Serial.println("l");
-  Serial.println(mapch1pwm);
-}
-else if(mapch1pwm>127)
-{
-  switchchar="r";
-  Serial.println("r");
-}
 
-else 
-{
-  //stope();
-  Serial.println("slr");
-}*/
-Serial.print(switchchar);
 }
-
+}
 void forward()
 {
+  Serial.println("forward");
   digitalWrite(motlf,HIGH);
   digitalWrite(motlb,LOW);
   digitalWrite(motrf,HIGH);
@@ -82,14 +76,15 @@ void forward()
 }
 void backward()
 {
+  Serial.println("backward");
   digitalWrite(motlf,LOW);
-  digitalWrite(motlb,HIGH
-  );
+  digitalWrite(motlb,HIGH);
   digitalWrite(motrf,LOW);
   digitalWrite(motrb,HIGH);
 }
 void right()
 {
+  Serial.println("right");
   digitalWrite(motlf,HIGH);
   digitalWrite(motlb,LOW);
   digitalWrite(motrf,LOW);
@@ -97,6 +92,7 @@ void right()
 }
 void left()
 {
+  Serial.println("left");
   digitalWrite(motlf,LOW);
   digitalWrite(motlb,HIGH);
   digitalWrite(motrf,HIGH);
